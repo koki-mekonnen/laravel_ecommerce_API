109ff921de2c543ccccee570574b20666f6502eb
Selamawit <ezanawittadele@gmail.com>
2024-12-26 15:12:16 +0300

    feat: add authentication middlewares

--- config/auth.php
+++ config/auth.php
@@ -35,41 +35,44 @@ return [
     |
     */
 
-    'guards' => [
-        'web' => [
-            'driver' => 'session',
-            'provider' => 'users',
-        ],
-    ],
 
-    /*
-    |--------------------------------------------------------------------------
-    | User Providers
-    |--------------------------------------------------------------------------
-    |
-    | All authentication guards have a user provider, which defines how the
-    | users are actually retrieved out of your database or other storage
-    | system used by the application. Typically, Eloquent is utilized.
-    |
-    | If you have multiple user tables or models you may configure multiple
-    | providers to represent the model / table. These providers may then
-    | be assigned to any extra authentication guards you have defined.
-    |
-    | Supported: "database", "eloquent"
-    |
-    */
+  'guards' => [
+    'web' => [
+        'driver' => 'session',
+        'provider' => 'users',
+    ],
+    'api' => [
+        'driver' => 'jwt',
+        'provider' => 'users',
+    ],
+    'superadmin' => [ // Guard for SuperAdmins
+        'driver' => 'jwt',
+        'provider' => 'superadmins',
+    ],
+    'merchant' => [ // Guard for Merchants
+        'driver' => 'jwt',
+        'provider' => 'merchants',
+    ],
+],
+
+
 
     'providers' => [
-        'users' => [
-            'driver' => 'eloquent',
-            'model' => env('AUTH_MODEL', App\Models\User::class),
-        ],
-
-        // 'users' => [
-        //     'driver' => 'database',
-        //     'table' => 'users',
-        // ],
+    'users' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\User::class,
     ],
+    'superadmins' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\SuperAdmin::class,
+    ],
+    'merchants' => [
+        'driver' => 'eloquent',
+        'model' => App\Models\Merchant::class,
+    ],
+],
+
+
 
     /*
     |--------------------------------------------------------------------------
